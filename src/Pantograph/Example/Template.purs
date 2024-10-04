52b641626261ae594e33acc20692e66bd6bfc0c4
Henry Blanchette <henry@monkeyface.com>
2024-10-04 11:57:25 -0400

    updated Template

--- src/Pantograph/Example/Template.purs
+++ src/Pantograph/Example/Template.purs
@@ -4,12 +4,31 @@ import Pantograph.Grammar
 import Pantograph.Tree
 import Prelude
 
+import Data.Generic.Rep (class Generic)
 import Data.Maybe (Maybe)
+import Data.Show.Generic (genericShow)
+import Pantograph.Pretty (class Pretty)
 import Pantograph.Utility (unimplemented)
 
-data S
+data S = S
 
-data D
+derive instance Generic S _
+
+instance Show S where
+  show x = genericShow x
+
+instance Pretty S where
+  pretty S = unimplemented "(Pretty S).pretty"
+
+data D = D
+
+derive instance Generic D _
+
+instance Show D where
+  show x = genericShow x
+
+instance Pretty D where
+  pretty D = unimplemented "(Pretty D).pretty"
 
 --------------------------------------------------------------------------------
 
