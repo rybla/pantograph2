a37070e667e4049ae62a4b1bee002d8e3767dc17
rybla <henry@monkeyface.com>
2024-11-20 13:48:56 -0500

    automatically derive up propagation rules

--- src/Pantograph/Library/DerivePropagationAdjRulesFromDerRules.purs
+++ src/Pantograph/Library/DerivePropagationAdjRulesFromDerRules.purs
@@ -5,6 +5,7 @@ import Pantograph.Language
 import Pantograph.Tree
 import Prelude
 
+import Data.Array as Array
 import Data.FunctorWithIndex (mapWithIndex)
 import Data.List (List, (:))
 import Data.List as List
@@ -25,33 +26,65 @@ propagationAdjRules =
           sortMVs = rule.sort # collectMVs
         in
           -- propagate down rules
-          [ makeAdjRule
-              -- freshen each sort metavar in output sort as a new sort change metavar which will be matched against in the down change of the adjust boundary here
-              ( (rule.sort # map V.expand # renameMVs (addPrefix "ch"))
-                  ↓
-                    d
-                  //
-                    (sortMVs # map (\x -> x /\ makeMetaVar x))
-                  %
-                    (kidMVs # map (\x -> makeMetaVar x))
-              )
-              ( d
-                  //
-                    (sortMVs # map (\x -> x /\ makeMetaVar (x # addSuffix "outer")))
-                  %
+          [ [ makeAdjRule
+                -- freshen each sort metavar in output sort as a new sort change metavar which will be matched against in the down change of the adjust boundary here
+                ( (rule.sort # map V.expand # renameMVs (addPrefix "ch")) ↓
+                    ( d // (sortMVs # map (\x -> x /\ makeMetaVar x)) %
+                        (kidMVs # map (\x -> makeMetaVar x))
+                    )
+                )
+                ( d // (sortMVs # map (\x -> x /\ makeMetaVar (x # addSuffix "outer"))) %
                     ( kidMVs `List.zip` rule.kids
                         # map
                             ( \(kidMV /\ { sort }) ->
-                                (sort # map V.expand # renameMVs (MV.addPrefix "ch"))
-                                  ↓
-                                    makeMetaVar kidMV
+                                (sort # map V.expand # renameMVs (MV.addPrefix "ch")) ↓
+                                  makeMetaVar kidMV
                             )
                     )
-              )
-              ( \(AdjSubst { chs, sorts: _, adjs }) -> pure
-                  { chs: chs # Map.toUnfoldable :: List _
-                  , sorts: sortMVs # map (\x -> (x # addSuffix "outer") /\ (chs MV.!! (x # addPrefix "ch") # outerEndpoint))
-                  , adjs: Map.toUnfoldable adjs :: List _
-                  }
-              )
-          ]
+                )
+                ( \(AdjSubst { chs, sorts: _, adjs }) -> pure
+                    { adjs: Map.toUnfoldable adjs :: List _
+                    , chs: chs # Map.toUnfoldable :: List _
+                    , sorts: sortMVs # map (\x -> (x # addSuffix "outer") /\ (chs MV.!! (x # addPrefix "ch") # outerEndpoint))
+                    }
+                )
+            ]
+          , rule.kids
+              # mapWithIndex
+                  ( \i _ ->
+                      makeAdjRule
+                        ( d // (sortMVs # map (\x -> x /\ makeMetaVar x)) %
+                            ( kidMVs `List.zip` rule.kids
+                                # mapWithIndex
+                                    ( \j (kidMV_j /\ { sort: sort_j }) ->
+                                        if i == j then
+                                          (sort_j # map V.expand # renameMVs (addPrefix "ch")) ↑
+                                            makeMetaVar kidMV_j
+                                        else
+                                          makeMetaVar kidMV_j
+                                    )
+                            )
+                        )
+                        ( (rule.sort # map V.expand # renameMVs (MV.addPrefix "ch")) ↑
+                            ( d // (sortMVs # map (\x -> x /\ makeMetaVar (x # addSuffix "inner"))) %
+                                ( kidMVs `List.zip` rule.kids
+                                    # mapWithIndex
+                                        ( \j (kidMV_j /\ { sort: sort_j }) ->
+                                            if i == j then
+                                              makeMetaVar kidMV_j
+                                            else
+                                              (sort_j # map V.expand # renameMVs (MV.addPrefix "ch")) ↓
+                                                makeMetaVar kidMV_j
+                                        )
+                                )
+                            )
+                        )
+                        ( \(AdjSubst { adjs, chs, sorts: _ }) -> pure
+                            { adjs: Map.toUnfoldable adjs :: List _
+                            , chs: chs # Map.toUnfoldable :: List _
+                            , sorts: sortMVs # map (\x -> (x # addSuffix "inner") /\ (chs MV.!! (x # addPrefix "ch") # outerEndpoint))
+                            }
+                        )
+                  )
+              # Array.fromFoldable
+          ] # fold
